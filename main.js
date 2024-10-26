62958cf98ec6b288041b77a899976fca5b6642d2
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-10-26 17:07:02 +0200

    Suite Cactus

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 /*=============== SHOW MENU ===============*/
 
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 /* Menu show */
 if(navToggle){
@@ -35,14 +35,22 @@ const blurHeader = () =>{
     const header = document.getElementById('header')
     // Add a class if the bottom offset is greater than 50 of the viewport
     this.scrollY >= 50 ? header.classList.add('blur-header') 
-                       : header.classList.remove('blur-header')
+    : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
+    const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+    }
+    Window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
